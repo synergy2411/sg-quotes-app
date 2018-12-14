a88ed4abf2fa321569b54ae964f7176ae9ef7cc6
synergy2411 <synergy2411@outlook.com>
2018-12-14 14:39:19 +0530

    alertCtrl added

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,6 @@
+import { LibraryPage } from './../library/library';
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import { IonicPage, NavParams, NavController, AlertController } from 'ionic-angular';
 import { IQuote } from '../../model/quotes.model';
 
 @IonicPage()
@@ -7,18 +8,41 @@ import { IQuote } from '../../model/quotes.model';
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage implements OnInit{
+export class QuotesPage implements OnInit {
 
-  quoteGroup : { category : string; quotes : IQuote[]; icon : string;};
+  quoteGroup: { category: string; quotes: IQuote[]; icon: string; };
 
-  constructor(private navParams : NavParams){}
+  constructor(private navParams: NavParams,
+    private navCtrl: NavController,
+    private alertCtrl: AlertController) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.quoteGroup = this.navParams.get('category');
   }
 
-  onFavorite(quote : IQuote){
-    console.log(quote);
+  onFavorite(quote: IQuote) {
+    const alert = this.alertCtrl.create({
+      title: "Favorite Quote",
+      subTitle: "Are you Sure?",
+      message: "Are you sure to make this quote your favorite?",
+      buttons: [{
+        text: "Yes, Please go ahead",
+        handler: () => {
+          console.log(quote);
+        }
+      }, {
+        text: "No, I changed my mind",
+        handler: () => {
+          console.log("Cancelled.");
+        }
+      }]
+    })
+    alert.present();
+
+  }
+
+  ionViewWillLeave() {
+    this.navCtrl.push(LibraryPage);
   }
 
 }
